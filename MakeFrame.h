2bc38321aa6ca771d6c0b0aaa043d4fdad2b4899
Dado Sutter <dadosutter@gmail.com>
2016-06-02 19:23:08 -0300

    new file:   MakeFrame.cpp 	new file:   MakeFrame.h

---
+++ MakeFrame.h
@@ -0,0 +1,25 @@
+#ifndef __MAKE_FRAME_H__
+#define __MAKE_FRAME_H__
+
+#include <opencv/cv.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <time.h>
+
+/*
+Function that makes a black frame with the interactors silhouettes
+filled with a filling image.
+
+control is a gray-scale control image, a picture of the empty interaction space
+
+interaction is a gray-scale picture of the filled interaction space
+
+fill is a color picture to fill the silhoutes in the resulting image, tipically, the other side of the projection wall
+
+threshold is a number between 0 and 255. Corresponding pixels in control and interaction will be used as masks if the absolute difference between them is larger or equal to threshold.
+
+filltype is 
+*/
+cv::Mat makeFrame(cv::Mat control, cv::Mat interaction, cv::Mat fill, const int filltype, int threshold);
+
+#endif
